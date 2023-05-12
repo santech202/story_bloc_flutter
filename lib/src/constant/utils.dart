8991331531850085d27a86a507d9fcbe8e932870
santech202 <santech202@gmail.com>
2023-05-12 16:45:03 -0400

    fix profile screen

--- lib/src/constant/utils.dart
+++ lib/src/constant/utils.dart
@@ -1,22 +1,22 @@
 class NetworkUtils {
-  static final urlBase = 'https://teling.app/';
+  static final urlBase = 'https://xxx.com';
   //static final urlBase = 'http://192.168.1.109:8000/';
   static final serverApi = 'api/';
   static final tokenEndpoint = 'oauth/token';
 
   static String clientIdentifier = '2';
-  static String clientSecret = 'q4xMgwCNtutgq7dC0b8kjgvzgxGtQvj8AXy0uZdl';
+  static String clientSecret = '';
 
   static String storageKeyMobileToken = 'token';
 
-  static const ADMOB_APP_ID_ANDROID = 'ca-app-pub-2613166030732333~1558890454';
-  static const ADMOB_APP_ID_IOS = 'ca-app-pub-2613166030732333~4505263826';
+  static const ADMOB_APP_ID_ANDROID = 'ca-app-pub-2613166030732333~xxxxxxxx';
+  static const ADMOB_APP_ID_IOS = 'ca-app-pub-2613166030732333~xxxxxxx';
 
   static const InterstitialAdUnitIdAndroid =
-      'ca-app-pub-2613166030732333/9513402093';
+      'ca-app-pub-2613166030732333/xxxxxxx';
   static const InterstitialAdUnitIdIOS =
-      'ca-app-pub-2613166030732333/9538098655';
+      'ca-app-pub-2613166030732333/xxxxxxxx';
 
-  static const BannerAdUnitIdAndroid = 'ca-app-pub-2613166030732333/9261464115';
-  static const BannerAdUnitIdIOS = 'ca-app-pub-2613166030732333/4911148429';
+  static const BannerAdUnitIdAndroid = 'ca-app-pub-2613166030732333/xxxxxxx';
+  static const BannerAdUnitIdIOS = 'ca-app-pub-2613166030732333/xxxxxxx';
 }
